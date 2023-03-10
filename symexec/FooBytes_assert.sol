af19a416ae115d9cda09a5a8a807b78e7a69da5f
baolean <baolean@protonmail.com>
2023-03-10 17:01:15 +0100

    Modified Foo for symexec experiments

---
+++ symexec/FooBytes_assert.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.17;
+
+contract FooBytes {
+    uint256 internal constant MAGIC = 0x69;
+
+    uint256 internal flagA = 0;
+    uint256 internal flagB = 0;
+
+    function foo(bytes32 x) external {
+        assembly {
+            if iszero(xor(x, MAGIC)) {
+                sstore(flagA.slot, 0x01)
+            }
+        }
+    }
+
+    function bar(bytes32 y) external {
+        assembly {
+            if iszero(xor(y, MAGIC)) {
+                sstore(flagB.slot, 0x01)
+            }
+        }
+    }
+
+    function revertIfCracked() external view {
+        bool res = true;
+        assembly {
+            if and(sload(flagA.slot), sload(flagB.slot)) {
+                res := false
+            }
+        }
+        assert(res);
+    }
+}
